fea301a9a3cfa404a8bfa6ff2a9f8827128fe1ab
jolenewai <jolene.wai@gmail.com>
2020-04-23 22:25:25 +0000

    basic update route created

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ from bson.objectid import ObjectId
 load_dotenv()
 
 DB_NAME = os.environ.get('DB_NAME')
+ratings = [1,2,3,4,5]
+
+
 
 app = Flask(__name__)
 
@@ -19,6 +22,18 @@ def index():
     users = client[DB_NAME].users.find()
     return render_template('index.template.html', reviews=reviews, users=users)
 
+
+@app.route('/add_review')
+def add_review():
+    return render_template('add_review.template.html')
+
+
+@app.route('add_review', methods=['POST'])
+def process_add_review():
+    return redirect(url_for('index'))
+
+
+
 @app.route('/edit_review/<review_id>')
 def edit_review(review_id):
     client = data.get_client()
@@ -27,12 +42,14 @@ def edit_review(review_id):
         '_id': ObjectId(review_id)
     })
 
-    print (review)
-    users = client[DB_NAME].users.find({
-        '_id': review[user_id]
+    user = client[DB_NAME].users.find({
+        '_id': review['user_id']
     })
 
-    return render_template('edit_review.template.html', r=review, u=user)
+    categories = client[DB_NAME].categories.find()
+
+
+    return render_template('edit_review.template.html', r=review, u=user, cat=categories, ratings=ratings)
 
 # "magic code" -- boilerplate
 if __name__ == '__main__':
